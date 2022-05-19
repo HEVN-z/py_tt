b2aef49d25d21003ea4f6be6afad5ee6591a8fee
HEVNz <e-exe-e@live.com>
2022-05-20 02:06:46 +0700

    g

---
+++ basic/var.py
@@ -0,0 +1,30 @@
+x = 10
+y = 5
+print(x + y)
+
+z = "Hello"
+print(z + " World")
+
+# print(x + y + z)
+# error because x and y is int cannot addition with string
+
+print(str(x) + str(y) + z) # convert x and y to strings and concatenate
+
+print(x, y, z) # comma to print multiple values without addition
+
+# operators
+# + addition
+# - subtraction
+# * multiplication
+# / division
+# % modulus
+# ** exponent
+# // floor division
+
+print(x + y)    # = 15
+print(x - y)    # = 5
+print(x * y)    # = 50
+print(x / y)    # = 2.0
+print(x % y)    # = 0
+print(x ** y)   # = 100000
+print(x // y)   # = 2
