0ed48352a8523732a7926c9ed167eadc1ae51753
HEVNz <e-exe-e@live.com>
2022-05-20 02:43:11 +0700

    g

---
+++ basic/03con.py
@@ -0,0 +1,17 @@
+# Conditionals
+
+if True:
+    print("True")
+
+if False:
+    print("False")
+
+# the if will do only True result
+
+if not True:
+    print("not True")
+
+if not False:
+    print("not False")
+
+# not is used to inverte the result
