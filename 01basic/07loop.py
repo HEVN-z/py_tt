414c531b9866e5d30a83ba3a0a0e189002496adc
HEVNz <e-exe-e@live.com>
2022-05-22 04:58:45 +0700

    g

--- 01basic/07loop.py
+++ 01basic/07loop.py
@@ -1,4 +1,28 @@
 i = 1
 while i <= 10:
-    print(i)
+    print('i =',i)
     i += 1
+
+# (start)           i = 1
+# while (stop):     i <= 10    if (stop) condition is false, the loop is stop
+#    (step)         i += 1
+
+for j in range(1,11): # step is 1 as default
+    print('j =',j)
+
+for j in range(1,11,1):
+    print('j =',j)
+
+# for j in range((start),(stop),(step))
+# if start < step then step
+# define j = start if j < stop then j += step
+#        j = 1 < 11 then j+= 1
+
+for k in range(0,12,2): # if step = 2
+    print('k =',k)
+
+for l in range(5,-21,-1): # if step = minus
+    print('\tL =',l)
+
+#
+#
