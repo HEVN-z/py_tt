f87ca4303a28d5eb9b170940aa243b4649611543
HEVNz <e-exe-e@live.com>
2022-05-22 05:18:31 +0700

    g

---
+++ 01basic/09module.py
@@ -0,0 +1,7 @@
+import _09module as cls
+cls.funcA() # prinht funcA
+
+print('aa =',cls.aa) # print aa = 1
+print('bb =',cls.bb) # print bb = 2
+print('cc =',cls.cc) # print cc = 3
+print('dd =',cls.dd) # print dd = 4
---
+++ 01basic/_09module.py
@@ -0,0 +1,7 @@
+aa = 1
+bb = 2
+cc = 3
+dd = 4
+
+def funcA():
+    print('funcA')
