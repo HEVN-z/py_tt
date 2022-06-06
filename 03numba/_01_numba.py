9ceba7f2049f2561daf2641f3a1177d14a7e8d0d
HEVNz <e-exe-e@live.com>
2022-06-06 20:04:52 +0700

    g

--- 03numba/_01_numba.py
+++ 03numba/_01_numba.py
@@ -1,10 +1,27 @@
-from numba import jit
+from numba import jit, cuda, njit
 import time
 
-@jit
+@njit
 def f(x, y):
     # A somewhat trivial example
     return x + y
+
+#@cuda.jit (Attempted fix #1)
+#@cuda.jit(device = True) (Attempted fix #2)
+#@cuda.jit(int32(int32,int32)) (Attempted fix #3)
+
+@njit
+def increment_by_one(an_array):
+    # Thread id in a 1D block
+    tx = cuda.threadIdx.x
+    # Block id in a 1D grid
+    ty = cuda.blockIdx.x
+    # Block width, i.e. number of threads per block
+    bw = cuda.blockDim.x
+    # Compute flattened index inside the array
+    pos = tx + ty * bw
+    if pos < an_array.size:  # Check array boundaries
+        an_array[pos] += 1
 
 start = time.time()
 print(f(1,2))
