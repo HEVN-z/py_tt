f8f7cf7abb076af7866921ba95b1ed974d7b5c05
HEVNz <e-exe-e@live.com>
2022-06-07 14:34:25 +0700

    Update _01time.py

--- 01_2time/_01time.py
+++ 01_2time/_01time.py
@@ -1,7 +1,7 @@
-from iqoptionapi.stable_api import IQ_Option
 import time
-bot = IQ_Option('dummy.esper@gmail.com', '12651265exe')
-bot.connect()
-server_time = bot.get_server_timestamp()
-final_time = time.strftime("%Y-%m-%d-%H:%M:%S", time.ctime(server_time))
-print(final_time)
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370)))
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+print(time.strftime('%H:%M:%S', time.localtime(time.time())))
