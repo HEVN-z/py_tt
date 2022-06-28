e238f10dac28a44bf2f75612860af9c52f66cd0e
HEVNz <e-exe-e@live.com>
2022-06-28 14:03:19 +0700

    g

--- 15OpenCV/01_opencv.py
+++ 15OpenCV/01_opencv.py
@@ -18,3 +18,38 @@ def returnCameraIndexes():
     return arr
 
 print(returnCameraIndexes())
+id = 0
+cap = cv2.VideoCapture(id)
+ret , frame = cap.read()
+
+# print(ret)
+# print(frame)
+
+plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+plt.show()
+
+cap.release()
+def take_photo(): 
+    cap = cv2.VideoCapture(id)
+    ret, frame = cap.read()
+    cv2.imwrite('webcamphoto.jpg', frame)
+    cap.release()
+
+take_photo()
+
+cap = cv2.VideoCapture(id)
+# Loop through every frame until we close our webcam
+while cap.isOpened(): 
+    ret, frame = cap.read()
+    
+    # Show image 
+    cv2.imshow('Webcam', frame)
+    
+    # Checks whether q has been hit and stops the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+
+# Releases the webcam
+cap.release()
+# Closes the frame
+cv2.destroyAllWindows()
